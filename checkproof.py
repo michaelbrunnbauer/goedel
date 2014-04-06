4f46fc9d3bf1105df3c19498b1d992c75b887f31
Michael Brunnbauer <brunni@netestate.de>
2014-04-06 12:31:48 +0200

    Update

--- checkproof.py
+++ checkproof.py
@@ -9,14 +9,25 @@ config.fastfunctions=True
 
 def checkproof(s):
     s=s.replace(' ','')
-    if isvalidproof(gn(s)) == 1:
-        return
     s=s.strip()
     s=s.split('\n')
+    s1=[]
+    for line in s:
+        if not line.startswith('#'):
+            s1.append(line)
+    s=s1
+    s1='\n'.join(s)
+    s1+='\n'
+    if isvalidproof(gn(s1)) == 1:
+        print "correct!"
+        return
     for x in range(1,len(s)+1):
         s1='\n'.join(s[:x])
         s1+='\n'
-        assert isvalidproof(gn(s1)) == 1,'\n'+s1
+        if isvalidproof(gn(s1)) == 0:
+            print "not correct at last line:"
+            print s1
+            return
 
 if not len(sys.argv):
     print "usage: checkproof.py file1, file2, ..."
